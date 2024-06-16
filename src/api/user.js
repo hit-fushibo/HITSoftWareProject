6b0e9cca3799c26418febf06214ccad6e9ab67ae
hit-fushibo <fushibo123123123@163.com>
2024-06-17 00:50:40 +0800

    封装登录和注册请求，测试成功

--- src/api/user.js
+++ src/api/user.js
@@ -13,3 +13,13 @@ export const userRegisterService=(registerData)=>{
     return requests.post('/user/register',params);
 }
 
+//提供注册接口
+export const userLoginService=(registerData)=>{
+    //借助urlSearchParams完成传递
+    const params= new URLSearchParams();
+    for(let key in registerData){
+        params.append(key,registerData[key]);
+    }
+    return requests.post('/user/login',params);
+}
+
