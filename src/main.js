4b6c496cdb01fdab2f392349db83e40dee6e2803
hit-fushibo <fushibo123123123@163.com>
2024-06-17 00:50:12 +0800

    设置路由

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ import ElementPlus from 'element-plus' //导入element-plus
 import 'element-plus/dist/index.css' //导入element-plus样式
 import App from './App.vue' //导入App.vue
 import locale from "element-plus/dist/locale/zh-cn.js"
+import router from '@/router'
 
 const app = createApp(App) //创建组件实例
 
+app.use(router)
 app.use(ElementPlus,{locale}) //使用element-plus
 app.mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,44 @@
+import {createRouter,createWebHistory} from "vue-router";
+
+//导入组件
+import LoginVue from "@/view/Login.vue"
+import mainPage from "@/view/mainPage.vue"
+import tree from "@/view/treeAndProcess/tree.vue"
+import requestProcess from "@/view/treeAndProcess/requestProcess.vue";
+import setting from "@/view/user/setting.vue";
+import password from "@/view/user/password.vue"
+import avatarUpload from "@/view/user/avatarUpload.vue";
+
+//定义路由关系
+const routes=[
+
+    //页面路由
+    {path:'/login',component:LoginVue},
+    {
+        path:'/',
+        component:mainPage,
+        redirect:'/treeAndProcess/tree',
+        children:[
+            {path: '/treeAndProcess/tree',component:tree},
+            {path: '/treeAndProcess/requestProcess',component:requestProcess},
+            {path: '/user/setting',component:setting},
+            {path: '/user/password',component:password},
+            {path: '/user/avatarUpload',component:avatarUpload}
+        ]
+        
+    }
+
+    //mainPage子路由
+
+]
+
+//创建路由器
+
+const router=createRouter({
+    history:createWebHistory(),
+    routes:routes
+})
+
+//导出路由器
+export default router
+
