a6930c00cfe2821411c1aa93d25d4705bae2e9af
hit-fushibo <fushibo123123123@163.com>
2024-06-18 02:05:18 +0800

    添加静态资源访问路由

--- src/main/java/com/example/springboot/config/WebConfig.java
+++ src/main/java/com/example/springboot/config/WebConfig.java
@@ -4,6 +4,7 @@ import com.example.springboot.interceptors.LoginInterceptors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -14,6 +15,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         //登录和注册不拦截
-        registry.addInterceptor(loginInterceptors).excludePathPatterns("/user/login","/user/register");
+        registry.addInterceptor(loginInterceptors).excludePathPatterns("/user/login","/user/register","/static/**");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
     }
 }
