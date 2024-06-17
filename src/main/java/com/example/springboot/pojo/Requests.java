737abf65dc6e287eb7736d19cd4028c0c2769285
hit-fushibo <fushibo123123123@163.com>
2024-06-17 18:07:09 +0800

    更改类设计

--- src/main/java/com/example/springboot/pojo/Requests.java
+++ src/main/java/com/example/springboot/pojo/Requests.java
@@ -1,16 +1,27 @@
 package com.example.springboot.pojo;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 
 @Data
 public class Requests {
     private String rid;
-    private String uid;
     private  String fromUid;
+    private String fromName;
     private String toUid;
-    private String  rType;
+    private String toName;
+    @JsonIgnore
+    private String  meOrOthers;//0-对自己树的申请，1-对他人树的申请
+    @JsonIgnore
+    private String type;//0-删除，1-增加，2-修改
+    @JsonIgnore
+    private String sOrt;//0-学生，1-老师
+    @JsonIgnore
     private String newLevel;
+    @JsonIgnore
     private String startTime;
+    @JsonIgnore
     private String endTime;
+    private String description;
 }
