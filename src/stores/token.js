32c30b4377c49a1a16be5f05889f2b106ea0688c
hit-fushibo <fushibo123123123@163.com>
2024-06-18 13:39:38 +0800

    用户信息存储

--- src/stores/token.js
+++ src/stores/token.js
@@ -7,7 +7,7 @@ defineStore参数描述：
 defineStore返回值描述：
 返回的是一个函数，将来可以调用该函数，得到第二个参数中返回的内容
 */
-export const useTokenStore = defineStore("token", () => {
+export const usrTokenStore = defineStore("token", () => {
     //1.定义描述token
     const token = ref("");
     //2.定义修改token的方法
@@ -22,6 +22,36 @@ export const useTokenStore = defineStore("token", () => {
         token,
         setToken,
         removeToken,
+    };
+},
+{
+    persist:true
+}
+);
+
+export const usrInfoStore = defineStore("usrInfo", () => {
+    //1.定义描述token
+    const usrInfo = ref({
+        uid:"",
+        phone:"",
+        email:"",
+        name:"",
+        nickname:"",
+        usrPic:"",
+        myPage:""
+    })
+    //2.定义修改token的方法
+    const setUsrInfo = (newUsrInfo) => {
+        usrInfo.value = newUsrInfo;
+    };
+    //3.定义移除token的方法
+    const removeUsrInfo = () => {
+        token.value = "";
+    };
+    return {
+        usrInfo,
+        setUsrInfo,
+        removeUsrInfo,
     };
 },
 {
